da78c9ec696da5dec918440e88d28ce09d1db944
Raj-Mandal-20 <usingrajcseinfo@gmail.com>
2024-07-04 01:02:10 +0530

    adding email setup

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -3,6 +3,7 @@ const { validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 const Request = require('../model/Request');
 const User = require('../model/User');
+const nodemailer = require("nodemailer");
 
 exports.createRequest = async (req, res, next)=>{
     
@@ -32,7 +33,6 @@ exports.createRequest = async (req, res, next)=>{
     email = user.email;
   })
   
-//   console.log(req.userId);
   
   try{
       const request = new Request({
@@ -58,5 +58,50 @@ exports.createRequest = async (req, res, next)=>{
         err.statusCode = 500;
       }
     next(err);
+  }
+}
+
+
+async function sendEMail() {
+
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_FROM,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+  
+  const recipients = ['hailchiku6@gmail.com', 'anioriginal4@gmail.com', 'souvikbh2@gmail.com', 'usingrajcseinfo@gmail.com'];
+  const mailOptions = {
+    from: `BeTheDonor <${process.env.EMAIL_FROM}>`,
+    to: recipients.join(','),
+    subject: "[URGENT] Blood Donation",
+    html: `
+      <h2> A Patient Needed O Negative (O-) Blood, Are You Available? </h2>
+    `,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email: ", error);
+    } else {
+      console.log("Email sent: ", info.response);
+    }
+  });
+}
+
+exports.notification = async(req, res, next)=>{
+  try{
+      await sendEMail();
+      res.status(200).json({
+          message : 'Broadcast email Successful'
+      });
+  }
+  catch(err){
+      console.log(err);
   }
 }
--- Backend/routes/feed.js
+++ Backend/routes/feed.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
-router.post('/create-request', feedController.createRequest)
+router.post('/create-request', isAuth,  feedController.createRequest);
+router.get('/notification', feedController.notification);
 
 
 module.exports = router;
